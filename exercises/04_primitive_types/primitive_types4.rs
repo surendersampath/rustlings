4bf219de462abc0773390f2f628603c96d33ed46
Suren <suresampats@gmail.com>
2026-08-05 23:37:32 +0100

    update progress

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,9 @@
 fn main() {
     // You can optionally experiment here.
+    let data = [ 10, 20, 30, 40, 50 ];
+
+    let data_slice = &data[2..4];
+    println!("data_slice has length {} and elements {:?}", data_slice.len(), data_slice);
 }
 
 #[cfg(test)]
@@ -9,7 +13,7 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
